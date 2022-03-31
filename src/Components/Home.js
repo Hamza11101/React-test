851f74cdf0ba4b7b932ecc54ee7c100deab586ec
Hamza Boubaker <hamzaboubaker21@gmail.com>
2022-03-31 14:44:07 +0200

    First-commit

---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (  
+        <div>
+            <h1 className="text-center">Create your account and see your location weather</h1>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+// import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+<nav className="navbar">
+<h1>React Test</h1>
+
+
+</nav>       
+
+      );
+}
+ 
+export default NavBar;
